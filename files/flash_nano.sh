4df4ecbed38fef54f74965ef74b0bf896842c842
Mario <bernhard@f1-gmbh.de>
2020-01-15 15:48:26 +0100

    add scripts to support automatic board installation

---
+++ files/flash_nano.sh
@@ -0,0 +1,134 @@
+#!/bin/sh
+
+##
+#	this is an script to flash the ls1012a-nano board
+#
+#	USAGE: flash_nano.sh [section]
+#
+#	examples:
+#		flash_nano.sh			: this will flash all images (MAC if set)
+#		flash_nano.sh RCW		: flash only the rcw image
+#		flash_nano.sh "RCW PFE"		: flash rcw image and pfe firmware
+#
+#	to flash the ethernet MAC addresses:
+#		PFE0_MAC=00:11:22:33:44:55 PFE1_MAC=00:22:33:44:55:66 flash_nano.sh MAC
+#
+#	or with all other images:
+#		PFE0_MAC=00:11:22:33:44:55 PFE1_MAC=00:22:33:44:55:66 flash_nano.sh
+#
+##
+
+SECTIONS=${1:-"RCW MAC UBOOT PFE"}
+
+# NOTE: must be absolute or relative to openocd working directory
+IMAGE_PATH=tmp-glibc/deploy/images/ls1012anano
+
+RCW_SECTOR_START=0
+RCW_SECTOR_END=0
+RCW_ADDRESS=0
+RCW_FILE=rcw/ls1012anano/rcw/ls1012anano_switch.bin
+
+MAC_SECTOR_START=4
+MAC_SECTOR_END=4
+MAC_ADDRESS=0x40000
+
+UBOOT_SECTOR_START=16
+UBOOT_SECTOR_END=24
+UBOOT_ADDRESS=0x100000
+UBOOT_FILE=u-boot-ls1012anano.bin-qspi
+
+PFE_SECTOR_START=160
+PFE_SECTOR_END=160
+PFE_ADDRESS=0xa00000
+PFE_FILE=engine-pfe-bin/pfe_fw_sbl.itb
+
+#\_________________________________________/#
+
+trap cleanup EXIT
+
+cleanup() {
+	if [ -n "${MAC_FILE}" -a -f "${MAC_FILE}" ]; then
+		rm ${MAC_FILE}
+	fi
+
+	if [ -n "${PID_OPENOCD}" ]; then
+		kill ${PID_OPENOCD}
+	fi
+}
+
+if [ ${IMAGE_PATH:0:1} != "/" -a ! -d ${IMAGE_PATH} ]; then
+
+	TMPDIR=$(find build* -maxdepth 1 -type d -name tmp-glibc)
+
+	if [ -n "${TMPDIR}" ]; then
+		cd ${TMPDIR%tmp-glibc}
+	else
+		echo "Please change directory to folder containing ${IMAGE_PATH}"
+		exit 1
+	fi
+fi
+
+if [ -z "${PFE_MAC0}" -a -z "${PFE_MAC1}" ]; then
+	SECTIONS=${SECTIONS/MAC}
+
+else
+	MAC_FILE=$(tempfile)
+
+	if [ -n "${PFE_MAC0}" ]; then
+		echo -en $(echo -n ${PFE_MAC0} | sed 's/://g;s/\(..\)/\\x\1/g') > ${MAC_FILE}
+	fi
+
+	if [ -n "${PFE_MAC1}" ]; then
+		echo -en $(echo -n ${PFE_MAC1} | sed 's/://g;s/\(..\)/\\x\1/g') >> ${MAC_FILE}
+	fi
+fi
+
+PATH=${PWD}/$(find tmp-glibc/sysroots -name openocd | sed -n '/bin\/openocd/{s/\/openocd//p}'):$PATH
+
+openocd -f interface/ftdi/olimex-arm-usb-ocd-h.cfg -f target/ls1012a.cfg &
+
+PID_OPENOCD=${!}
+
+
+# S25FS512S - disable 4-kB Erase (CR3NV - Uniform Sector Architecture)
+# TODO fslqspi read_register 0 0x04 != 0x08
+(echo "halt; fslqspi write_register 0 0x04 0x08;") | telnet localhost 4444
+
+
+for S in ${SECTIONS}; do
+
+	START=$(VN=${S}_SECTOR_START; echo ${!VN})
+ 	END=$(VN=${S}_SECTOR_END; echo ${!VN})
+	ADDR=$(VN=${S}_ADDRESS; echo ${!VN})
+	FILE=$(VN=${S}_FILE; echo ${!VN})
+
+	if [ ${FILE:0:1} != '/' ]; then
+		FILE=${IMAGE_PATH}/${FILE}
+	fi
+
+	FILE_SIZE=$(stat -L -c %s ${FILE}) || exit
+
+	(echo "halt;" \
+	      "flash protect 0 ${START} ${END} off;" \
+	      "flash erase_sector 0 ${START} ${END};" \
+	      "fslqspi endian 0 BE32;" \
+	      "flash write_bank 0 ${FILE} ${ADDR};") \
+	| nc localhost 4444 \
+	| while read LINE; do
+
+		if [ "${LINE:0:5}" = "error" -o "${LINE:0:16}" = "Nothing to write" ]; then
+
+			echo "Failed to write ${FILE} to ${ADDR}"
+			exit 1
+
+		else
+			SUCCESS="wrote ${FILE_SIZE} bytes from file"
+
+			if [ "${LINE:0:${#SUCCESS}}" = "${SUCCESS}" ]; then
+				break
+			fi
+		fi
+	done
+
+done
+
---
+++ files/flash_setup.sh
@@ -0,0 +1,165 @@
+#!/bin/sh
+
+###
+#
+# You may setup variables:
+# IP_INSTALL	- the ip address used by u-boot
+# IP_NANO	- the ip address used by install image (may be IP_INSTALL)
+# IP_SERVER	- the bootp server ip address
+# IP_NETMASK	- ip netmask
+#
+# IMAGE_INSTALL - the install image name
+# IMAGE_BASE	- the image name, to install on emmc
+#
+###
+
+if [ -n "$(which openocd)" ]; then
+	echo -e "Warning: found openocd in PATH. This version may not work with ls1012a cpu\n"
+	A=n
+	read -p "Continue? [y/N]:" A
+
+	if [ -n "${A}" -a "${A}" != "y" -a "${A}" != "Y" ]; then
+		exit 1
+	fi
+fi
+
+PATH=${PWD}/$(find build/tmp-glibc/sysroots -name openocd | sed -n '/bin\/openocd/{s/\/openocd//p}'):$PATH
+
+if [ -z "$(which openocd)" ]; then
+	MISSING="Failed to find openocd - Please build with: bitbake openocd-native\n"
+fi
+
+if [ -z "$(which atftpd)" ]; then
+	MISSING="${MISSING}Failed to find atftpd binary - Please install atftpd package (ie. apt-get install atftpd)"
+fi
+
+if [ -z "$(which bootpd)" ]; then
+	MISSING="${MISSING}Failed to find bootpd binary - Please install bootp package (ie. apt-get install bootp)"
+fi
+
+if [ -n "${MISSING}" ]; then
+	echo -e "${MISSING}"
+	exit 1
+fi
+
+
+kill_service() {
+	SPID=${1}
+	SNAME=${2}
+	CMDLINE=$(cat /proc/${SPID}/cmdline)
+
+	if [ "${CMDLINE}" != "${CMDLINE/${SNAME}}" ]; then
+		kill ${SPID}
+	fi
+}
+
+ERROR=1
+
+trap cleanup EXIT
+
+cleanup() {
+
+	if [ ${ERROR} -gt 0 ]; then
+
+		if [ -n "${PID_ATFTPD}" ]; then
+			kill_service ${PID_ATFTPD} atftpd
+		fi
+
+		if [ -n "${PID_BOOTPD}" ]; then
+			kill_service ${PID_BOOTPD} bootpd
+		fi
+
+		if [ -n "${BOOTPTAB}" ]; then
+			rm ${BOOTPTAB}
+		fi
+	fi
+}
+
+# Network variables
+
+if [ -z "${IP_SERVER}" ]; then
+
+	IP_SERVER=$(ip addr show eth0 | sed -n '/inet /{s/.*inet \([0-9.]*\).*/\1/;p}')
+
+	if [ -z "${IP_SERVER}" ]; then
+
+		echo "Failed to get the bootpd server address - Please set the IP_SERVER environment variable"
+		exit
+
+	fi
+
+fi
+
+if [ -z "${IP_INSTALL}" ]; then
+
+	H=${IP_SERVER##*.}
+
+	[ ${H} -gt 250 ] \
+		&& H=$((H - 1)) \
+		|| H=$((H + 1))
+
+	IP_INSTALL=${IP_SERVER%.*}.${H}
+fi
+
+[ -z "${IP_NANO}" ] \
+	&& IP_NANO=${IP_INSTALL}
+
+[ -z "${IP_NETMASK}" ] \
+	&& IP_NETMASK=255.255.255.0
+
+[ -z "${IMAGE_INSTALL}" ] \
+	&& IMAGE_INSTALL=itbImage-ls1012anano-install-ls1012anano.bin
+
+[ -z "${IMAGE_BASE}" ] \
+	&& IMAGE_BASE=core-image-minimal-ls1012anano.tar.gz
+
+
+# atftpd setup
+
+echo "Starting tftp daemon..."
+
+sudo atftpd --daemon ${PWD}/build/tmp-glibc/deploy/images/ls1012anano || exit
+
+PID_ATFTPD=${!}
+
+if [ -z "${PID_ATFTPD}" ]; then
+	for I in $(find /proc -maxdepth 1 -type d -name "[0-9]*"); do
+		if [ $(grep -c "^atftpd" ${I}/cmdline) -gt 0 ]; then
+			PID_ATFTPD=${I##*/}
+			break
+		fi
+	done
+fi
+
+
+# bootpd setup
+
+BOOTPTAB=$(tempfile)
+
+cat << EOF > ${BOOTPTAB}
+install:\\
+        :ht=ethernet:\\
+        :ha=001122334455:\\
+        :ip=${IP_INSTALL}:\\
+        :sm=${IP_NETMASK}:\\
+        :sa=${IP_SERVER}:\\
+        :bf=${IMAGE_INSTALL}:
+
+nano:\\
+        :ht=ethernet:\\
+        :ha=001122334456:\\
+        :ip=${IP_NANO}:\\
+        :sm=${IP_NETMASK}:\\
+        :sa=${IP_SERVER}:\\
+        :bf=${IMAGE_BASE}:
+EOF
+
+echo "Starting bootp daemon..."
+
+sudo bootpd -d -s ${BOOTPTAB} &
+
+PID_BOOTPD=${!}
+
+ERROR=0
+
+exit 0
