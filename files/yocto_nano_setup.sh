131fd215d0e5100b2619e0326ef715b53265a75b
Mario <bernhard@f1-gmbh.de>
2020-01-31 10:01:00 +0100

    yocto_nano_setup.sh: add missing update

--- files/yocto_nano_setup.sh
+++ files/yocto_nano_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Branches: yocto bitbake
-Y=thud
-B=1.40
+YBRANCH=zeus
+BBRANCH=1.44
 
 # yocto core
 # http://www.openembedded.org/wiki/OE-Core_Standalone_Setup
@@ -11,25 +11,21 @@ git init || exit 1
 git remote add origin git://git.openembedded.org/openembedded-core || exit 1
 git fetch || exit 1
 
-git checkout ${Y} || exit 1
+git checkout ${YBRANCH} || exit 1
 
 git clone git://git.openembedded.org/bitbake bitbake || exit 2
 
-git -C bitbake checkout ${B} || exit 2
+git -C bitbake checkout ${BBRANCH} || exit 2
 
 # meta-openembedded
 git clone git://git.openembedded.org/meta-openembedded || exit 3
 
-git -C meta-openembedded checkout ${Y} || exit 3
+git -C meta-openembedded checkout ${YBRANCH} || exit 3
 
 # meta-freescale
 git clone git://git.yoctoproject.org/meta-freescale || exit 4
 
-git -C meta-freescale checkout ${Y} || exit 4
-
-if [ "${Y}" != "thud" ]; then
-	git -C meta-nano checkout ${Y} || exit 5
-fi
+git -C meta-freescale checkout ${YBRANCH} || exit 4
 
 # mata-nano & meta-freescale in die bblayers.conf.sample eintragen
 [ $(grep -c meta-nano meta/conf/bblayers.conf.sample) -eq 0 ] \
